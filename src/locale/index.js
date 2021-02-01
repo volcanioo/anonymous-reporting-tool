407a98cb2febd206fbc493d85190f74e577c326f
volcanioo <arayuzgelistirici@gmail.com>
2021-02-01 23:40:45 +0100

    ✨ first setup of the project

---
+++ src/locale/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import VueI18n from 'vue-i18n'
+
+function loadLocaleMessages() {
+  const locales = require.context(
+    "./",
+    true,
+    /[A-Za-z0-9-_,\s]+\.json$/i
+  );
+  const messages = {};
+  locales.keys().forEach(key => {
+    const matched = key.match(/([A-Za-z0-9-_]+)\./i);
+    if (matched && matched.length > 1) {
+      const locale = matched[1];
+      messages[locale] = locales(key);
+    }
+  });
+  return messages;
+}
+
+Vue.use(VueI18n)
+
+export default new VueI18n({
+  locale: "en",
+  fallbackLocale: "en",
+  messages: loadLocaleMessages()
+});
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import { auth } from './firebase/index';
+import i18n from './locale/index';
+
+Vue.config.productionTip = false;
+
+new Vue({
+  i18n,
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount('#app');
