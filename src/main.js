b11e707520fc9b47cbed927700a1838418b08691
volcanioo <arayuzgelistirici@gmail.com>
2021-02-02 19:53:45 +0100

    🎨  language structure prepared and readme updated

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import { auth } from './firebase/index';
 import i18n from './locale/index';
 
 Vue.config.productionTip = false;
