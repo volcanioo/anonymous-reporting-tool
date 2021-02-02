86543d269aa01076edde5b5d54dee9f9ca272592
volcanioo <arayuzgelistirici@gmail.com>
2021-02-02 23:38:25 +0100

    ✨ homepage html development

---
+++ src/components/BaseSlider.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="slider">
+    <span class="slider__title">
+      <strong>Anonymously</strong> send feedback to us.
+    </span>
+    <p class="slider__description">
+      This tool allows the employees to report harassment/discrimination anonymously to the company.
+    </p>
+    <button class="slider__button">
+      Send Feedback
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseSlider",
+}
+</script>
+
+<style lang="scss" scoped>
+$element_offset: 75px;
+
+.slider {
+  width: 768px;
+  margin: 0 auto;
+  text-align: center;
+}
+
+.slider__title {
+  font-size: 36px;
+  line-height: 44px;
+  font-weight: 500;
+}
+
+.slider__title strong {
+  font-weight: 700;
+}
+
+.slider__description {
+  font-size: 20px;
+  line-height: 32px;
+  font-weight: 400;
+  padding: 0 $element_offset;
+  margin: 20px 0 40px 0;
+}
+
+.slider__button {
+  width: calc(100% - #{$element_offset});
+  background: var(--dark-black);
+  margin: 0 auto;
+  color: white;
+  border: none;
+  height: 68px;
+  font-size: 20px;
+  line-height: 28px;
+  cursor: pointer;
+  transition: all .3s;
+
+  &:hover {
+    background: var(--secondary-color);
+    color: black;
+    transition: background .4s cubic-bezier(0.25, 1, 0.5, 1) .1s, box-shadow .2s cubic-bezier(0.25, 1, 0.5, 1) .5s;
+    box-shadow: 0px 0px 0px 6px var(--dark-black);
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,24 @@
 <template>
   <div class="send-feedback">
     <base-header></base-header>
+    <base-slider></base-slider>
+    <div class="send-feedback__footer">
+      <strong>{{ $t('scroll_down') }}</strong>
+      <img src="@/assets/mouse.svg" alt="Scroll down to learn how it works">
+    </div>
   </div>
 </template>
 
 <script>
 // import { collections } from '../firebase/index';
 import BaseHeader from '../components/BaseHeader.vue';
+import BaseSlider from '../components/BaseSlider.vue';
 
 export default {
   name: 'Homepage',
   components: {
     BaseHeader,
+    BaseSlider,
   },
   created() {
     // collections.unknown.doc().set({
@@ -24,8 +31,43 @@ export default {
 
 <style lang="scss">
 .send-feedback {
-  background: var(--primary-color);
+  background: var(--primary-color) url('../assets/face-bg-left.svg') no-repeat left center / contain;
   width: 100%;
   height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  &::after {
+    content: "";
+    background: url('../assets/face-bg-right.svg') no-repeat right center / contain;
+    display: block;
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    top: 0;
+    left: 0;
+    pointer-events: none;
+    z-index: 0;
+  }
 }
+
+.send-feedback__footer {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  strong {
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 28px;
+    margin-bottom: 12px;
+  }
+
+  img {
+    margin-bottom: 60px;
+  }
+}
+
 </style>
