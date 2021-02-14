f4e970d561556d73bb936d4b502a8681685b105a
Mehmet V. Deveci <arayuzgelistirici@gmail.com>
2021-02-14 14:28:15 +0100

    ♻️  logout function refactored

--- src/api/companies/logout.js
+++ src/api/companies/logout.js
@@ -1,12 +1,5 @@
 import { auth } from '../../firebase';
 
 export default function () {
-  return new Promise((resolve, reject) => {
-    auth.signOut()
-    .then((response) => {
-      resolve(response);
-    }).catch((error) => {
-      reject(error);
-    })
-  });
+  return auth.signOut();
 }
